fb5f4cd8bf2a7f24afd9ab7da211941c70f861d5
Graham-Langridge <grahamlangridge@lineone.net>
2018-06-06 15:21:56 +0100

    Implemented ToFriendlyString

--- TestRolesEnumMap/Program.cs
+++ TestRolesEnumMap/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TestRolesEnumMap
@@ -25,14 +26,46 @@ namespace TestRolesEnumMap
 
 		static void Main()
 		{
-			var something = Enum.GetValues(typeof(EnumAllUsers))
-				.Cast<EnumAllUsers>()
-				.Select(r => Enum.GetName(r.GetType(), r));
+			//var something = ListOfRoleNames2();
 
-			Console.WriteLine(string.Join(", ", something));
+			Console.WriteLine(string.Join(", ", ListOfRoleNames()));
+			Console.WriteLine(string.Join(", ", ListOfRoleNames2()));
 
 			//OR CTRL+F5 ;-)
 			Console.ReadLine();
+		}
+
+		private static IEnumerable<string> ListOfRoleNames()
+		{
+			return Enum.GetValues(typeof(EnumAllUsers))
+				.Cast<EnumAllUsers>()
+				.Select(r => Enum.GetName(r.GetType(), r));
+		}
+
+		private static IEnumerable<string> ListOfRoleNames2()
+		{
+			return (from EnumAllUsers enumAllUser
+					in Enum.GetValues(typeof(EnumAllUsers))
+					select ToFriendlyString(enumAllUser)).ToList();
+		}
+
+		public static string ToFriendlyString(EnumAllUsers me)
+		{
+			switch (me)
+			{
+				case EnumAllUsers.AdminGroup:
+					return AccessPermissions.AdminGroup;
+				case EnumAllUsers.CostingsTeamUserGroup:
+					return AccessPermissions.CostingsTeamUserGroup;
+				case EnumAllUsers.CostingsTeamManagerGroup:
+					return AccessPermissions.CostingsTeamManagerGroup;
+				case EnumAllUsers.MaterialTeamUserGroup:
+					return AccessPermissions.MaterialTeamUserGroup;
+				case EnumAllUsers.ReadOnlyUserGroup:
+					return AccessPermissions.ReadOnlyUserGroup;
+				default:
+					return string.Empty;
+			}
 		}
 	}
 }
